d500502b8ebd689d23093f2ae26099efe3b6a8bd
Rostislav Navratil <r.navratil@outlook.com>
2026-03-22 20:41:08 -0700

    Fix download URL filename

--- Casks/semafor.rb
+++ Casks/semafor.rb
@@ -2,7 +2,7 @@ cask "semafor" do
   version "1.0.0"
   sha256 "a3fbb87a7b36e71d3ff7b7262d81c63ee81589fba8ca79ed2e9c7dcdeaa1d5e6"
 
-  url "https://github.com/rnavratil/semafor/releases/download/v#{version}/Semafor.app.zip"
+  url "https://github.com/rnavratil/semafor/releases/download/v#{version}/Semafor.zip"
   name "Semafor"
   desc "Menu bar status indicator with CLI support"
   homepage "https://github.com/rnavratil/semafor"
